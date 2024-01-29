507415447affc5ed17f62af3439e45dd5fa12926
Rodrigo Recio <rodrigo@recio.com>
2024-01-29 02:02:32 -0300

    Initial Commit

---
+++ Owera/OweraApp.swift
@@ -0,0 +1,32 @@
+//
+//  OweraApp.swift
+//  Owera
+//
+//  Created by Rodrigo Recio on 1/29/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct OweraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
